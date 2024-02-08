dc5eebc2d819ead8fa89cebcd8380792ebab2a56
Jonas Reventlow Petersen <jonnep96@hotmail.com>
2024-02-08 14:58:09 +0100

    Delete Idle/Alder.py

--- Idle/Alder.py
+++
@@ -1,8 +0,0 @@
-#Øvelse 2.a
-age = 22
-age_input = int(input("Indtast din alder:"))
-
-print("Variablen age er af typen", type(age))
-print("Variablen age_input er af typen", type(age_input))
-
-      
